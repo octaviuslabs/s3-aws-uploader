9d0257251fb0cbfeb6d199e60c08d0383349f498
jjcody <jackcody@gmail.com>
2015-01-06 11:38:44 -0800

    - Cleaned up JavaScript code - Changed names of certain selectors

--- lib/s3_aws_uploader/view_helpers.rb
+++ lib/s3_aws_uploader/view_helpers.rb
@@ -4,7 +4,7 @@ module S3AwsUploader
     def s3_upload_field(input_field, s3_upload_controller_path)
       input_field = input_field.to_s.gsub('-', '_')
       
-      input_field = input_field.to_s # + '_upload'
+      input_field = input_field.to_s
       return output = %[<input id="#{input_field}" name="file" type="file" data-path="#{s3_upload_controller_path}" style="display: none;" />]
     end
 
@@ -18,7 +18,7 @@ module S3AwsUploader
     def bootstrap_s3_upload(input_field, s3_upload_controller_path)
       output = %[<div id="#{input_field}_container" class="file-input-container">]
       output += %[<div id="#{input_field}_status">]
-      output += %[<button id="#{input_field}_choose_file" type="button" class="#{S3AwsUploader.config.button_style}">choose file</button><div class="file-upload-filename">No file selected.</div>]
+      output += %[<button id="#{input_field}_choose_file" type="button" class="#{S3AwsUploader.config.button_style}">choose file</button><div id="#{input_field}_filename" class="file-upload-filename">No file selected.</div>]
       output += %[</div>]
       output += s3_upload_field(input_field, s3_upload_controller_path)
       output += progress_bar(input_field)
